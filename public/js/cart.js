90868f36b7ca752b8f53acb0d2ea7aacc87b7fdb
Yevhenii <kysil_jenia@ukr.net>
2020-06-12 15:54:28 +0200

    Added cart of page, new goods
    added new categories with goods. Added cart of page, fixed bugs, some styles

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,8 +1,12 @@
 let cart = {};
-
 document.querySelectorAll('.add-to-cart').forEach(function(element){
     element.onclick = addToCart;
 });
+
+if(localStorage.getItem('cart')) {
+    cart = JSON.parse(localStorage.getItem('cart'));
+    ajaxGetGoodsInfo();
+}
 
 function addToCart(){
     let goodsId = this.dataset.goods_id;
@@ -17,6 +21,7 @@ function addToCart(){
 }
 
 function ajaxGetGoodsInfo(){
+    updateLocalStorageCart();
     fetch('/get-goods-info', {
         method: 'POST',
         body: JSON.stringify({key: Object.keys(cart)}),
@@ -26,9 +31,55 @@ function ajaxGetGoodsInfo(){
         }
     })
     .then(function(res){
-
+        return res.text();
     })
     .then(function(body){
         console.log(body);
+        showCart(JSON.parse(body));
     });
+}
+
+function showCart(data) {
+    let out = '<table class="table table-striped table-cart"><tbody>';
+    let total = 0;
+    for (let key in cart) {
+        out += `<tr><td colspan = "4"><a href="/goods?id=${key}">${data[key]['name']}</a></tr>`;
+        out += `<tr><td><i class = "far fa-minus-square cart-minus" data-goods_id="${key}"></i></td>`;
+        out += `<td>${cart[key]}</td>`;
+        out += `<td><i class = "far fa-plus-square cart-plus" data-goods_id="${key}"></i></td>`;
+        out += `<td>${data[key]['cost']*cart[key]} pln</td>`;
+        out += '</tr>';
+        total += cart[key]*data[key]['cost'];
+    }
+    out += `<tr><td colspan = "3">Razem: </td><td>${total} pln</td></tr>`;
+    out += '</tbody></table>';
+    document.querySelector('#card-nav').innerHTML = out;
+    document.querySelectorAll('.cart-minus').forEach(function(element){
+        element.onclick = cartMinus;
+    });
+    document.querySelectorAll('.cart-plus').forEach(function(element){
+        element.onclick = cartPlus;
+    });
+}
+
+function cartPlus() {
+    let goodsId = this.dataset.goods_id;
+    console.log(goodsId);
+    cart[goodsId]++;
+    ajaxGetGoodsInfo();
+}
+
+function cartMinus() {
+    let goodsId = this.dataset.goods_id;
+    if(cart[goodsId] -1 > 0){
+        cart[goodsId]--;
+    }
+    else {
+        delete(cart[goodsId]);
+    }
+    ajaxGetGoodsInfo();
+}
+
+function updateLocalStorageCart() {
+    localStorage.setItem('cart', JSON.stringify(cart));
 }
